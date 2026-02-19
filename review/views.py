7a6d7ab793b22f9b089fc79a6c23ec6bc5fb7ee4
ChenZhao <2381401246@qq.com>
2026-02-19 02:15:39 +0000

    update

--- review/views.py
+++ review/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+def review():
+    return 0
+
+def review_share():
+    return 0
+
+def review_report():
+    return 0
+
+def review_edit():
+    return 0
+
+def review_delete():
+    return 0
+
+def review_score():
+    return 0
+
+def item_list():
+    return 0
+
+def item_search():
+    return 0
+
+@login_required
+def my_review():
+    return 0
