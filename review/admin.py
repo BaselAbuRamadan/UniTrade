7f785881e192c86b96edf1201d31482129a7ccc6
ChenZhao <2381401246@qq.com>
2026-02-19 02:15:39 +0000

    update

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Register your models here.
+def administrator():
+    return 0
+
+def admin_user_management():
+    return 0
+
+def admin_item_management():
+    return 0
+
+def admin_report_management():
+    return 0
+
+def admin_review_management():
+    return 0
+
+def admin_notification_management():
+    return 0
+
+def admin_permission_management():
+    return 0
