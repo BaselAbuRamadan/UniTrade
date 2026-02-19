93a13a6c3ef8cdc6706693b87d2916d51f43c850
ChenZhao <2381401246@qq.com>
2026-02-19 02:09:20 +0000

    update

--- item/views.py
+++ item/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+def item():
+    return 0
+
+def item_publish():
+    return 0
+
+def item_edit():
+    return 0
+
+def item_delete():
+    return 0
+
+def item_category():
+    return 0
+
+def item_tag():
+    return 0
+
+def item_list():
+    return 0
+
+def item_search():
+    return 0
+
+@login_required
+def my_products():
+    return 0
